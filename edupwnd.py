78dea811991fd6ea60745a7572c44fb4a092400d
SKittish <zvonko2779@gmail.com>
2020-04-05 21:12:35 +0200

    added stuff + edupage web

--- edupwnd.py
+++ edupwnd.py
@@ -1,26 +1,11 @@
-#Script for pwding edupage from the cookie XSS vunerability cookies
+#Script for pwding edupage from the XSS vunerability
 #Usage: 
-''' Example header 
 
-{"Response Headers (508 B)":
-    {"headers":
-        [
-            {"name":"Cache-Control","value":"no-store, no-cache, must-revalidate"},
-            {"name":"Cache-Control","value":"post-check=0, pre-check=0"},
-            {"name":"Connection","value":"Keep-Alive"},
-            {"name":"Content-Length","value":"0"},
-            {"name":"Content-Type","value":"text/html; charset=UTF-8"},
-            {"name":"Date","value":"Fri, 03 Apr 2020 16:47:51 GMT"},
-            {"name":"Expires","value":"Mon, 26 Jul 1997 05:00:00 GMT"},
-            {"name":"Keep-Alive","value":"timeout=1, max=100"},
-            {"name":"Last-Modified","value":"Fri, 03 Apr 2020 16:47:51 GMT"},
-            {"name":"Location","value":"https://ssnovohradska.edupage.org/user/?ESID=d65b85bf621eea0f787c9c0fb801c65b&hsid=b27dfb1bcc26345250300d80536304f4db9c0a0e"},
-            {"name":"Pragma","value":"no-cache"},{"name":"Server","value":"Apache"}]}}
-'''
 import requests
 import sys
 import webbrowser
 import getopt
+import flask
 
 
 #functinality
@@ -30,6 +15,12 @@ import getopt
 
 #list of all possible usage cases and flags
 possibilities = {
+    "download": {
+        "desc":"downloads all nessecary files for setup",
+        "flags": {
+            "-d":"download dir"
+        }  
+    },
     "generate" : {
         "desc":"Generates malicious url + js for sniffing use, built for specific port & user url",
         "flags": {
@@ -38,29 +29,30 @@ possibilities = {
             "-n":"name of js script (default: 4 random characters)"
         }
     },
-    "translate": {
-        "desc":"Generates output cookie based on provided input cookie",
-        "flags": {
-            "-c":"cookie input"
-        }
-    },
     "server"   : {
         "desc":"Hosts local server on select port listning to",
         "flags": {
             "-p":"listening port",
-            "-l":"log file"
+            "-l":"log file (default: none)"
         }
     },
     "genserve"     : {
         "desc":"generates malicious link and then creates listning server",
         "flags": {
             "-r":"url where listening server will be set up (default: localhost)",
+            "-P":"port to which JS sends (same as '-P')",
             "-p":"port of listening server (default: 80)",
             "-n":"name of js script (default: random characters)",
             "-l":"log file"
+            
         }
     }
 }
+
+#class arguments:
+ #   def __init__(self, args):
+        
+    
 
 
 class bcolors:
@@ -88,12 +80,18 @@ def print_help():
             print(f"\t  {flag}\t{possibilities[mode]['flags'][flag]}")
 
         print()
+    
 
 
+def print_log(msg):
+    print(f"[{bcolors.OKWHITE}!] LOG: {msg}")
 
 
 def print_warning(msg):
     print(f"[{bcolors.WARNING}!{bcolors.OKWHITE}] {bcolors.WARNING}WARNING:{bcolors.OKWHITE} {msg}")
+
+def print_error(msg):
+    print(f"[{bcolors.FAIL}!{bcolors.OKWHITE}] {bcolors.FAIL}ERROR:{bcolors.OKWHITE} {msg}")
 
 def print_logo():
     print(f"""{bcolors.OKBLUE}
@@ -109,15 +107,60 @@ def print_logo():
     print("")
     print_warning("""legal disclaimer, Usage of EDUPWND for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program""")
     print("")
-    print("HELP: --help / -h")
+    print("HELP: --help / -h\n\n")
+
+def validate_flags(mode, flags):
+    flag_output = []
+    for i in range(len(flags)):
+        if i%2==0:
+            if not flags[i] in possibilities[mode]["flags"].keys():
+                print_error(f"\"{flags[i]}\" is not a valid flag! To see valid flags use -h / --help")
+            else:
+                if i+1 == len(flags):
+                    print_error(f"no argument provided for flag\"{flags[i]}\"  for help use: -h / --help")
+                else:
+                    flag_output += [flags[i],flags[i+1]]
+    return flag_output
 
 
 
+
+def generate():
+    pass
+def download():
+    pass
+def server():
+    pass
+def genserve():
+    pass
+def handleInputs(input):
+    
+    if ("-h" in input) or ("--help" in input):
+        print_help()
+        exit()
+    if (len(input) < 2 or not input[1] in possibilities.keys()):
+        print_error("NO mode provided, please use -h / --help for help")
+        exit()
+    mode = input[1]
+    print_log(f"starting EDUPWND with mode {mode}")
+    flags=input[2:]
+    flag_pairs = validate_flags(mode,flags)
+    print(flag_pairs)
+    if mode == "download":
+        pass
+    elif mode == "generate":
+        pass
+    elif mode == "server":
+        pass
+    elif mode == "genserve":
+        pass
+    
 
 def main(argv):
    print_logo()
-   print_help()
+   handleInputs(argv)
+   #print_help()
    #for()
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main(sys.argv)
